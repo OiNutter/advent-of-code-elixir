dc012269488703f309aa81072206264cdf12c515
Will McKenzie <willmckenzie@oinutter.co.uk>
2025-12-02 08:59:58 +0000

    revert day 2 part 1 to faster string split solution

--- lib/advent_of_code/solution/year_2025/day_02.ex
+++ lib/advent_of_code/solution/year_2025/day_02.ex
@@ -8,12 +8,15 @@ defmodule AdventOfCode.Solution.Year2025.Day02 do
       #IO.inspect({start_str, end_str})
       start = String.to_integer(start_str)
       ending = String.to_integer(end_str)
-      Enum.map(start..ending, &Integer.to_string/1)
-      |> Enum.filter(fn id ->
-        String.match?(id, ~r/^(.+)\1$/)
+      Enum.map(start..ending, fn x ->
+        str_x = Integer.to_string(x)
+        String.split_at(str_x, str_x |> String.length() |> div(2))
+      end)
+      |> Enum.filter(fn {left, right} ->
+        left == right
       end)
     end)
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn {left, right} -> String.to_integer(left <> right) end)
     |> Enum.sum()
   end
 
