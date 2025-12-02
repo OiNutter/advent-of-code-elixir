c2e3e551f236f2c0eecd8ae6eacd6926907bdc99
Will McKenzie <willmckenzie@oinutter.co.uk>
2025-12-02 15:24:21 +0000

    more speed improvements for day 2

--- lib/advent_of_code/solution/year_2025/day_02.ex
+++ lib/advent_of_code/solution/year_2025/day_02.ex
@@ -3,20 +3,23 @@ defmodule AdventOfCode.Solution.Year2025.Day02 do
     input
     |> String.replace("\n", "")
     |> String.split(",", trim: true)
-    |> Enum.flat_map(fn range_str ->
+    |> Enum.map(fn range_str ->
       [start_str, end_str] = String.split(range_str, "-", trim: true)
       #IO.inspect({start_str, end_str})
-      start = String.to_integer(start_str)
-      ending = String.to_integer(end_str)
-      Enum.map(start..ending, fn x ->
-        str_x = Integer.to_string(x)
-        String.split_at(str_x, str_x |> String.length() |> div(2))
+      String.to_integer(start_str)..String.to_integer(end_str)
+    end)
+    |> Task.async_stream(fn range ->
+      range
+      |> Enum.map(fn x ->
+        str_x = Integer.to_charlist(x)
+        Enum.split(str_x, str_x |> length() |> div(2))
       end)
       |> Enum.filter(fn {left, right} ->
         left == right
       end)
     end)
-    |> Enum.map(fn {left, right} -> String.to_integer(left <> right) end)
+    |> Enum.flat_map(fn {:ok, v} -> v end)
+    |> Enum.map(fn {left, right} -> Enum.concat(left, right) |> List.to_integer() end)
     |> Enum.sum()
   end
 
@@ -24,13 +27,15 @@ defmodule AdventOfCode.Solution.Year2025.Day02 do
     input
     |> String.replace("\n", "")
     |> String.split(",", trim: true)
-    |> Enum.flat_map(fn range_str ->
+    |> Enum.map(fn range_str ->
       [start_str, end_str] = String.split(range_str, "-", trim: true)
-      start = String.to_integer(start_str)
-      ending = String.to_integer(end_str)
-      Enum.filter(start..ending, fn i ->
-        s = Integer.to_string(i)
-        l = String.length(s)
+      String.to_integer(start_str)..String.to_integer(end_str)
+    end)
+    |> Task.async_stream(fn range ->
+      range
+      |> Enum.filter(fn i ->
+        s = Integer.to_charlist(i)
+        l = length(s)
         if l < 2 do
           false
         else
@@ -38,14 +43,15 @@ defmodule AdventOfCode.Solution.Year2025.Day02 do
             if rem(l, i) != 0 do
               false
             else
-              s0 = String.slice(s, 0..(i-1))
-              s == String.duplicate(s0, div(l, i))
+              s0 = Enum.slice(s, 0..(i-1))
+              s == (List.duplicate(s0, div(l, i)) |> List.flatten())
             end
           end)
           |> Enum.any?()
         end
       end)
     end)
+    |> Enum.flat_map(fn {:ok, v} -> v end)
     |> Enum.sum()
   end
 end
