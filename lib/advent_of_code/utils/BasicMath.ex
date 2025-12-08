5b104b5873adacced1509eab0e50a16152b6baea
Will McKenzie <willmckenzie@oinutter.co.uk>
2025-12-08 15:56:16 +0000

    add manhattan distance helper to BasicMath module

--- lib/advent_of_code/utils/BasicMath.ex
+++ lib/advent_of_code/utils/BasicMath.ex
@@ -13,4 +13,8 @@ defmodule BasicMath do
   def euclidean_distance_3d({x1, y1, z1}, {x2, y2, z2}) do
     :math.sqrt(:math.pow(x2 - x1, 2) + :math.pow(y2 - y1, 2) + :math.pow(z2 - z1, 2))
   end
+
+  def manhattan_distance({sx, sy}, {tx, ty}) do
+    abs(sx - tx) + abs(sy - ty)
+  end
 end
